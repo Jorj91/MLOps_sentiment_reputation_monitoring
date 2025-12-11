7c56ed555e322ea6e4d7cdeea0b2582068782b1d
Jorj91 <43264875+Jorj91@users.noreply.github.com>
2025-12-11 17:53:05 +0000

    Convert hf_deploy and backup to normal folders and add all contents

---
+++ hf_deploy_backup_20251211_122355/README.md
@@ -0,0 +1,9 @@
+---
+title: Mlops Sentiment Api
+emoji: 🌖
+colorFrom: pink
+colorTo: blue
+sdk: docker
+pinned: false
+app_file: app.py
+---
---
+++ hf_deploy_backup_20251211_122355/app.py
@@ -0,0 +1,48 @@
+# FastAPI application for serving the model
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sentiment import predict_sentiment
+import json
+
+app = FastAPI(title="Sentiment Monitoring API")
+
+class TextInput(BaseModel):
+    text: str
+
+
+# Lightweight monitoring
+
+stats = {
+    "total_requests": 0,
+    "prediction_counts": {"negative": 0, "neutral": 0, "positive": 0}
+}
+
+def log_prediction(label: str):
+    stats["total_requests"] += 1
+    if label in stats["prediction_counts"]:
+        stats["prediction_counts"][label] += 1
+
+def save_stats():
+    with open("stats.json", "w") as f:
+        json.dump(stats, f)
+
+# -----------
+@app.get("/")
+def home():
+    return {"message": "Welcome to MachineInnovators Sentiment API"}
+
+@app.post("/predict")
+def predict(input_data: TextInput):
+    result = predict_sentiment(input_data.text)
+    log_prediction(result["sentiment"])
+    save_stats()
+    return result
+
+
+@app.get("/stats")
+def get_stats():
+    return stats
+
+# source .venv/bin/activate
+# uvicorn src.app:app --reload --host 0.0.0.0 --port 8000
